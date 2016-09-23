a4cac7150805d5274684d03b2ad58046dbff8b51
umita <umita@10.50.137.38>
2016-09-23 11:47:18 +0200

    a

---
+++ src/main/webapp/javascript.js
@@ -0,0 +1,38 @@
+document.getElementById("div1").style.backgroundColor="red";
+document.getElementById("div1").style.height="100px";
+document.getElementById("div1").style.width="100px";
+
+document.getElementById("div2").style.backgroundColor="blue";
+document.getElementById("div2").style.height="100px";
+document.getElementById("div2").style.width="100px";
+
+document.getElementById("div3").style.backgroundColor="orange";
+document.getElementById("div3").style.height="100px";
+document.getElementById("div3").style.width="100px";
+
+
+
+
+function div1Click(){
+    
+document.getElementById("div1").addEventListener("Click",hideDiv1());
+}
+
+
+function hideDiv1(){
+document.getElementById("div1").style.backgroundColor="";
+document.getElementById("div1").style.height="20px";
+document.getElementById("div1").style.width="20px";
+ 
+}
+
+function div2Hover(){
+    document.getElementById("div2").addEventListener("mouseover",div2Hov());
+    }
+
+
+function div2Hov(){
+    document.getElementById("div2").style.borderHeight="4px";
+    document.getElementById("div2").style.borderWidth="4px";
+    
+}
